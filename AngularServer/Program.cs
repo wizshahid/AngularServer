be5cdc07cd9e80504bff0885822218cc7aa050ee
Shahid <wizshahid@gmail.com>
2022-04-18 11:55:27 +0530

    Update Program.cs

--- AngularServer/Program.cs
+++ AngularServer/Program.cs
@@ -6,7 +6,21 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowOrigin",
+        builder =>
+        {
+            builder.WithOrigins("http://localhost:4200")
+                   .AllowAnyHeader()
+                   .AllowAnyMethod();
+        });
+});
+
+
 var app = builder.Build();
+
+app.UseCors("AllowOrigin");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
